61ac53bc68cf1c853e5e3c4e8117fb5b59905f58
RafaelgbCarvalho <carvalhorafael2702@gmail.com>
2025-02-26 21:39:27 -0300

    Update Program.cs

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Calculadora
+namespace Calculadora
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float numero1, numero2;
+            
+            Console.WriteLine("Digite o primeiro numero: ");
+            numero1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            numero2 = float.Parse(Console.ReadLine());
+            float subtracao = numero1 - numero2;    
+            float soma = numero1 + numero2;
+            Console.WriteLine("O resultado final da soma é: " + soma);
+            Console.WriteLine("A subtração dos numero é:" + subtracao);
         }
     }
 }
